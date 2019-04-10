1b6c3daaef8229e762c982c292717299d6847116
MAKSYM_enko <finside41@gmail.com>
2019-04-10 10:05:00 +0300

    Updates

---
+++ precache-manifest.a845554ff5b34d17fc478d86c9bad21a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d595c1f2f62de080ab0",
+    "url": "/login-app/static/js/runtime~main.8a02c311.js"
+  },
+  {
+    "revision": "273b258af1ef7042b9f6",
+    "url": "/login-app/static/js/main.9afded96.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/login-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "273b258af1ef7042b9f6",
+    "url": "/login-app/static/css/main.cdfe4b91.chunk.css"
+  },
+  {
+    "revision": "97a3e233f9c3fae9dc65e7c0c1136b91",
+    "url": "/login-app/index.html"
+  }
+];
