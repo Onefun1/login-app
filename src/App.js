ad757655bc227b7619aa5eee6aff5db5d1882196
MAKSYM_enko <finside41@gmail.com>
2019-04-11 21:00:54 +0300

    API_URL was added, home page remuwe and host is used

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import { getUsers } from "./api/users";
 
 class App extends Component {
+  componentDidMount() {
+    getUsers().then(console.log);
+  }
+
   render() {
-    return <div className="App">Hello World!</div>;
+    return <div className="App">Hello 1234</div>;
   }
 }
 
