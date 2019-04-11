d45b74a82b86f4f41da8267454ce57b93cd5191f
MAKSYM_enko <finside41@gmail.com>
2019-04-11 22:34:57 +0300

    NavLink

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { getUsers } from "./api/users";
+import { NavLink } from "react-router-dom";
 
 class App extends Component {
   componentDidMount() {
@@ -8,7 +9,76 @@ class App extends Component {
   }
 
   render() {
-    return <div className="App">Hello 1234</div>;
+    return (
+      <div className="App">
+        <header>
+          <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+            <a className="navbar-brand" href="/">
+              Mate academy
+            </a>
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarCollapse"
+              aria-controls="navbarCollapse"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/" exact>
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/sign-in">
+                    Sign in
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/sign-up">
+                    Sign up
+                  </NavLink>
+                </li>
+              </ul>
+
+              <div className="mt-2 mt-md-0">
+                <ul className="nav nav-pills">
+                  <li className="nav-item">
+                    <NavLink to="/sign-in" className="nav-link">
+                      Sign in
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink to="/sign-up" className="nav-link">
+                      Sign up
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </header>
+
+        <main role="main" className="container">
+          <h1>Home Page</h1>
+        </main>
+
+        <footer className="footer">
+          <div className="container">
+            <span className="text-muted">
+              Place sticky footer content here.
+            </span>
+          </div>
+        </footer>
+      </div>
+    );
   }
 }
 
